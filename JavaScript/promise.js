e58c8bdb6f927300df00c20b57719789e266581f
KRISH GUPTA <krishgupta2806@gmail.com>
2026-09-03 14:51:26 +0530

    callbackhell n promise

---
+++ JavaScript/promise.js
@@ -0,0 +1,30 @@
+function f1(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("hi");
+        resolve();
+    },4000)
+    })
+
+}
+
+function f2(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("ABES ENGINEERING COLLEGE");
+        resolve();
+    },2000)
+    })
+
+}
+
+async function a() {
+    try {
+        await f1();
+        await f2();
+    } catch (err) {
+        console.log("ERRORRR", err);
+    }
+}
+
+a();
